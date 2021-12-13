6f607471b437863e300f2ec3ccc23f29f9e747e4
shaikapon <shai.kapon@gigaspaces.com>
2021-12-13 18:48:37 +0200

    adding app

---
+++ precache-manifest.fc119e35b43a8585bf768beccc9c0098.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ebbdbaa343ff2574fc57d8890777b96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a176a63a2c07d9acbb8",
+    "url": "/static/css/2.1a7e3916.chunk.css"
+  },
+  {
+    "revision": "b81f3dc194a847113c1c",
+    "url": "/static/css/main.3804d6c4.chunk.css"
+  },
+  {
+    "revision": "7a176a63a2c07d9acbb8",
+    "url": "/static/js/2.b7655682.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "/static/js/2.b7655682.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b81f3dc194a847113c1c",
+    "url": "/static/js/main.7ddc19ee.chunk.js"
+  },
+  {
+    "revision": "32762759838a60329c5c",
+    "url": "/static/js/runtime-main.8ac6ae30.js"
+  }
+]);
